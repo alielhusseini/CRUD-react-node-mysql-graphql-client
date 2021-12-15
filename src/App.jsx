242fc87a62c7ffe680f2f10a520589bd4bc63fa8
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-12-15 16:51:14 +0200

    committing grapql client side

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_USER } from './graphql/graphql'
 
 function App() {
+  const [profile, setProfile] = useState({name: "", username: "", password: ""})
+  const [createUser, { error }] = useMutation(CREATE_USER, {
+    variables: profile,
+    update(proxy, result) {
+      console.log('inserted')
+    }
+  })
+
+  const handleChange = e => setProfile({...profile, [e.target.name]: e.target.value})
+  console.log(profile)
   return (
     <div className="App">
-      hello world
+      <div className="createUser">
+        <input onChange={ handleChange } value={ profile.name } type="text" name="name" id="name" placeholder="Name..."/>
+        <input onChange={ handleChange } value={ profile.username } type="text" name="username" id="username" placeholder="Username..."/>
+        <input onChange={ handleChange } value={ profile.password } type="text" name="password" id="password" placeholder="Password..."/>
+        <button onClick={ createUser }>Create User</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './index.css';
+import { InMemoryCache, ApolloProvider, ApolloClient } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/graphql', // reqs to this end point (the graphql server) / we didn' use thge HttpLink nor the setContext('@apollo/client/link/context')
+  cache: new InMemoryCache()
+});
 
 render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
